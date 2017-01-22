f9e899b877756cdabfdca8493837740e9c8c7a59
Linus <linusg.97@hotmail.com>
2017-01-22 12:34:58 +0100

    lägga till recept och början på lägga till ingredienser

--- js/script.js
+++ js/script.js
@@ -13,6 +13,10 @@ module.config(function ($urlRouterProvider, $stateProvider) {
         url: "/register",
         templateUrl: "templates/register.html",
         controller:"registerCtrl"
+    }).state("addrecept", {
+        url: "/addrecept",
+        templateUrl: "templates/addrecept.html",
+        controller:"addreceptCtrl"
     });
 });
 
@@ -24,9 +28,15 @@ module.controller("homeCtrl", function ($scope, ReceptService) {
     });
 });
 
+module.controller("addreceptCtrl", function ($scope, $rootScope, ReceptService) {    
+    $scope.addRecept = function () {
+        ReceptService.addRecept($scope.r_namn,$scope.instruktion,Number($scope.author),$scope.kategori,$scope.imglink);
+    };
+});
+
 module.controller("registerCtrl", function ($scope, ReceptService){    
     $scope.register = function () {
-        ReceptService.register();
+        ReceptService.register($scope.username, $scope.password);
     };
 });
 
@@ -62,5 +72,54 @@ module.service("ReceptService", function ($q, $http, $rootScope) {
            console.log("Du kan inte logga in"); 
         });
     };
+    
+    this.register = function (username, password) {
+        var data = {
+            username:username,
+            password: password
+        };
+        var url = "http://localhost:8080/projekt4git/webresources/register";
+
+        $http({
+            url: url,
+            method: "POST",
+            data:data
+        }).then(function (data) {
+            console.log("Du är registrerad");
+            $rootScope.isRegistrerad = true;
+            $rootScope.user = username;
+            $rootScope.pass = password;
+        },function(data){
+            alert("Testa med ett annat användarnamn eller försök igen senare!");
+           console.log("Du kan inte registrera dig"); 
+        });
+    };
+    
+    this.addRecept = function (r_namn, instruktion, author, kategori, imglink) {
+        var data = {
+            r_namn:r_namn,
+            instruktion:instruktion,
+            author:author,
+            kategori:kategori,
+            imglink:imglink     
+        };
+        var url = "http://localhost:8080/projekt4git/webresources/Rec";
+        var auth = "Basic " + window.btoa($rootScope.user + ":" + $rootScope.pass);
+        $http({
+            url: url,
+            method: "POST",
+            data:data,
+            headers: {'Authorization': auth}
+        }).then(function (data) {
+            alert("Du har lagt till ett recept!Lägg nu till vilka ingredienser!");
+            console.log("Du la till et recept");
+            $rootScope.isRecept = true;
+        },function(data){
+            alert("xD");
+           console.log("du la inte till ett recept"); 
+        });
+    };
+    
+    
 });
 
